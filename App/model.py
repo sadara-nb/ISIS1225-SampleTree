a54c13812a6457c6352c69a682c135cd0ea45153
miguel parra <ma.parrat@uniandes.edu.co>
2023-10-05 17:40:29 -0500

    Fix: modify comparefunction to cmpfucntion

--- App/model.py
+++ App/model.py
@@ -58,7 +58,7 @@ def newAnalyzer():
 
     analyzer['crimes'] = lt.newList('SINGLE_LINKED', compareIds)
     analyzer['dateIndex'] = om.newMap(omaptype='BST',
-                                      comparefunction=compareDates)
+                                      cmpfunction=compareDates)
     return analyzer
 
 
